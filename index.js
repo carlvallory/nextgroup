1f982aa5b3e76ab63987e02a66d1ba14ceaac0f0
Vallory <carlvallory@gmail.com>
2023-08-21 10:16:20 -0400

    0.0.14

--- index.js
+++ index.js
@@ -207,6 +207,7 @@ async function getSendMsg(id, body, msgObj) {
         author = msgObj.msg.author;        
     }
 
+    console.log(msgObj.msg.from.user, body);
     const sendMessageData = await client.sendMessage(msgObj.msg.from.user, body);
 
     return sendMessageData;
